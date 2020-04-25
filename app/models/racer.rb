ff06d4f02da1b3ff7666aa8d705670e075d99c36
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-25 18:42:37 +0800

    Refactored Racer#roll

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -6,29 +6,16 @@ class Racer < ApplicationRecord
   belongs_to :race
 
   def roll roll, speed: nil
-    case speed
-    when :NORMAL
-      move = ((roll.odd? ? 1 : 2) - self.damage)
-    when :SUPER
-      move = (roll - self.damage)
-      self.damage += 2
-    end
+    move, damage = calculate roll, speed: speed
 
-    move = [move, 0].max
+    move = [(move - self.damage), 0].max
 
     self.position += move
+    self.damage += damage
+
     self.position = [self.position, self.race.finish_line].min
 
-    self.rolls.create race:         self.race,
-                      position:     self.position_was,
-                      damage:       self.damage_was,
-                      speed:        speed,
-                      roll:         roll,
-                      move:         move,
-                      new_position: self.position,
-                      new_damage:   self.damage,
-                      crashed:      self.crashed?,
-                      won:          self.won?
+    log roll, move, speed
 
     save
   end
@@ -40,4 +27,32 @@ class Racer < ApplicationRecord
   def crashed?
     self.damage >= MAX_DAMAGE
   end
+
+  private
+
+    def calculate roll, speed:
+      case speed
+      when :NORMAL
+        move = (roll.odd? ? 1 : 2)
+        damage = 0
+      when :SUPER
+        move = roll
+        damage = 2
+      end
+
+      [move, damage]
+    end
+
+    def log roll, move, speed
+      self.rolls.create race:         self.race,
+                        position:     self.position_was,
+                        damage:       self.damage_was,
+                        speed:        speed,
+                        roll:         roll,
+                        move:         move,
+                        new_position: self.position,
+                        new_damage:   self.damage,
+                        crashed:      self.crashed?,
+                        won:          self.won?
+    end
 end
