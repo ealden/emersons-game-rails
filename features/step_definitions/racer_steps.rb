34ecee4512f85a5d93727cdccefe4632b0aea4fd
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-25 15:11:43 +0800

    GIVEN I see the finish line at position {int}

--- features/step_definitions/racer_steps.rb
+++ features/step_definitions/racer_steps.rb
@@ -10,3 +10,7 @@ end
 Given 'I have damage of {int}' do |damage|
   @racer.damage = damage
 end
+
+Given 'I see the finish line at position {int}' do |finish_line|
+  @race.finish_line = finish_line
+end
