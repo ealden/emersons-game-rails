f88f7a7e3a20f9dcbdd5742e1b4c05a779b6fb58
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-26 16:31:34 +0800

    Seed using Race.new_race

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
-Race.delete_all
-
-race = Race.create
-race.update finish_line: 10
-
-race.racers.create name: 'Alice'
-race.racers.create name: 'Bob'
-race.racers.create name: 'Charlie'
-race.racers.create name: 'Dave'
+Race.new_race
