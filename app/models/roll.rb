8a9e2af4a912ae57d9ec48450e9aed509fa0895b
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-26 12:54:50 +0800

    Roll#damaged?

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,4 +1,10 @@
 class Roll < ApplicationRecord
+  NO_DAMAGE = 0
+
   belongs_to :race
   belongs_to :racer
+
+  def damaged?
+    self.damage.present? and (self.damage > NO_DAMAGE)
+  end
 end
--- spec/models/roll_spec.rb
+++ spec/models/roll_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Roll, type: :model do
+  describe :damaged? do
+    it 'must be false if no damage' do
+      roll = Roll.new
+
+      expect(roll).not_to be_damaged
+    end
+
+    it 'must be true if at least 1 damage' do
+      roll = Roll.new
+      roll.damage = 1
+
+      expect(roll).to be_damaged
+    end
+  end
 end
