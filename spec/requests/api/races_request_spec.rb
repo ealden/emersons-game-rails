aa67a8e8d3fb840b4e2eb57a67e2a054718b234f
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-28 17:35:04 +0800

    Extract expected JSON to own files

--- spec/requests/api/races_request_spec.rb
+++ spec/requests/api/races_request_spec.rb
@@ -1,151 +1,50 @@
-require 'rails_helper'
-
 RSpec.describe Api::RacesController, type: :request do
+  let(:race) { Race.create }
+
+  before do
+    race.update finish_line: 10
+  end
+
   describe 'GET /api/races' do
     it 'must return current race' do
-      race = Race.create
-      race.update finish_line: 10
-
       racer = race.racers.create name: 'Alice'
-
-      expected_response = {
-        id: 1,
-        racers: [
-          {
-            id: 1,
-            name: 'Alice',
-            position: 0,
-            damage: 0,
-            rank: 1,
-            winner: false,
-            crashed: false
-          }
-        ],
-        currentRacer: {
-          id: 1,
-          name: 'Alice',
-          position: 0,
-          damage: 0,
-          rank: 1,
-          winner: false,
-          crashed: false
-        },
-        lastRoll: nil,
-        finishLine: 10,
-        allCrashed: false,
-        over: false
-      }
 
       get '/api/races'
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eql expected_response.to_json
+      expect(response.json).to eql 'api/races/get_response'.json
     end
 
-    it 'must be allCrashed if all racers crashed' do
-      race = Race.create
-      race.update finish_line: 10
-
+    it 'must set allCrashed to true if all racers crashed' do
       racer = race.racers.create name: 'Alice'
       racer.roll 1, speed: :NORMAL
       racer.update damage: Racer::MAX_DAMAGE
 
-      expected_response = {
-        id: 1,
-        racers: [
-          {
-            id: 1,
-            name: 'Alice',
-            position: 1,
-            damage: 6,
-            rank: 1,
-            winner: false,
-            crashed: true
-          }
-        ],
-        currentRacer: {
-          id: 1,
-          name: 'Alice',
-          position: 1,
-          damage: 6,
-          rank: 1,
-          winner: false,
-          crashed: true
-        },
-        lastRoll: {
-          id: 1,
-          racer: {
-            id: 1,
-            name: 'Alice',
-            position: 1,
-            damage: 6,
-            rank: 1,
-            winner: false,
-            crashed: true
-          },
-          position: 0,
-          damage: 0,
-          speedType: 'NORMAL',
-          roll: 1,
-          move: 1,
-          newPosition: 1,
-          newDamage: 0,
-          crashed: false,
-          win: false,
-          damaged: false,
-          normalSpeed: true,
-          superSpeed: false
-        },
-        finishLine: 10,
-        allCrashed: true,
-        over: true
-      }
-
       get '/api/races'
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eql expected_response.to_json
+      expect(response.json).to eql 'api/races/get_with_all_racers_crashed_response'.json
     end
 
     it 'must render even if no racers' do
-      race = Race.create
-      race.update finish_line: 10
-
-      expected_json = {
-        id: 1,
-        racers: [],
-        currentRacer: nil,
-        lastRoll: nil,
-        finishLine: 10,
-        allCrashed: false,
-        over: false
-      }
-
       get '/api/races'
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eql expected_json.to_json
+      expect(response.json).to eql 'api/races/get_with_no_racers_response'.json
     end
   end
 
   describe 'GET /api/races/settings' do
     it 'must return race settings' do
-      expected_response = {
-        testMode: true
-      }
-
       get '/api/races/settings'
 
       expect(response).to have_http_status :ok
-      expect(response.body).to eql expected_response.to_json
+      expect(response.json).to eql 'api/races/settings/get_response'.json
     end
   end
 
   describe 'POST /api/races/roll' do
     it 'must roll for racer' do
-      race = Race.create
-      race.update finish_line: 10
-
       race.racers.create name: 'Alice'
       race.racers.create name: 'Bob'
 
@@ -156,77 +55,14 @@ RSpec.describe Api::RacesController, type: :request do
 
       expect(response).to have_http_status :ok
 
-      expected_json = {
-        id: 1,
-        racers: [
-          {
-            id: 1,
-            name: 'Alice',
-            position: 1,
-            damage: 0,
-            rank: 1,
-            winner: false,
-            crashed: false
-          },
-          {
-            id: 2,
-            name: 'Bob',
-            position: 0,
-            damage: 0,
-            rank: 2,
-            winner: false,
-            crashed: false
-          }
-        ],
-        currentRacer: {
-          id: 2,
-          name: 'Bob',
-          position: 0,
-          damage: 0,
-          rank: 2,
-          winner: false,
-          crashed: false
-        },
-        lastRoll: {
-          id: 1,
-          racer: {
-            id: 1,
-            name: 'Alice',
-            position: 1,
-            damage: 0,
-            rank: 1,
-            winner: false,
-            crashed: false
-          },
-          position: 0,
-          damage: 0,
-          speedType: 'NORMAL',
-          roll: 1,
-          move: 1,
-          newPosition: 1,
-          newDamage: 0,
-          crashed: false,
-          win: false,
-          damaged: false,
-          normalSpeed: true,
-          superSpeed: false
-        },
-        finishLine: 10,
-        allCrashed: false,
-        over: false
-      }
-
       get '/api/races'
 
-      expect(response.body).to eql expected_json.to_json
+      expect(response.json).to eql 'api/races/get_after_roll_response'.json
     end
   end
 
   describe 'POST /api/races/new' do
     it 'must create a new race' do
-      race = Race.create
-      racer = race.racers.create name: 'Alice'
-
       post '/api/races/new', params: {}
 
       expect(response).to have_http_status :ok
