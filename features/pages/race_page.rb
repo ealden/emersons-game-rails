305a94b154b53824e9da70305bc1a8f7dcfb8577
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-28 17:31:44 +0800

    Formatting

--- features/pages/race_page.rb
+++ features/pages/race_page.rb
@@ -1,18 +1,16 @@
 class RacePage < SitePrism::Page
   set_url '/'
 
-  element :roll_field,        '#test-roll'
-  element :processing_field,  '#test-processing'
-
-  element :message_field, '#message'
+  element :roll_field,          '#test-roll'
+  element :processing_field,    '#test-processing'
+  element :message_field,       '#message'
 
   element :normal_speed_button, '#roll-normal-speed'
   element :super_speed_button,  '#roll-super-speed'
   element :new_race_button,     '#new-race'
 
-  element :race_controls, '#race-controls'
-  element :race_over,     '#race-over'
-
+  element :race_controls,       '#race-controls'
+  element :race_over,           '#race-over'
 
   def roll roll, speed:
     roll_field.set roll
