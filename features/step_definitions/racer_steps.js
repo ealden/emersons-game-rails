48f04cfe892aa2f323bf68239adf8fec8c1bbdc2
Ealden Esto E. Escañan <ealden@escanan.com>
2020-08-08 16:24:43 +0800

    Initial racer_steps.js

---
+++ features/step_definitions/racer_steps.js
@@ -0,0 +1,79 @@
+const { Given, When, Then } = require('cucumber');
+
+Given('I am in a race', function () {
+});
+
+Given('I am at position {int}', function (position) {
+});
+
+Given('I have damage of {int}', function (damage) {
+});
+
+Given('I see the finish line at position {int}', function (finishLine) {
+});
+
+When('I choose {string} speed', function (speed) {
+});
+
+When('I roll a {int}', function (roll) {
+});
+
+When('I choose to start over in a new race', function () {
+});
+
+When('I try to view the race', function () {
+});
+
+When('all racers have crashed!', function () {
+});
+
+When("it's over, it's over", function () {
+});
+
+Then('I must now be at position {int}', function (newPosition) {
+});
+
+Then('I must now have damage of {int}', function (newDamage) {
+});
+
+Then('I must see the race result: --', function () {
+});
+
+Then('I must see the race result: WIN', function () {
+});
+
+Then('I must now have a log entry with the following:', function () {
+});
+
+Then('Position: {int}', function (position) {
+});
+
+Then('Damage: {int}', function (damage) {
+});
+
+Then('Speed: {string}', function (speed) {
+});
+
+Then('Roll: {int}', function (roll) {
+});
+
+Then('Move: {int}', function (move) {
+});
+
+Then('New Position: {int}', function (newPosition) {
+});
+
+Then('New Damage: {int}', function (newDamage) {
+});
+
+Then('Crashed: {string}', function (crashed) {
+});
+
+Then('Win: {string}', function (win) {
+});
+
+Then('I must see the message: {string}', function (message) {
+});
+
+Then('our race must be over!', function () {
+});
