66682f7110cd760457b719ecaa1bda108cf51fbc
Ealden Esto E. Escañan <ealden@escanan.com>
2020-12-12 02:17:54 +0800

    Rails 6.1

--- Gemfile
+++ Gemfile
@@ -3,32 +3,32 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.2'
 
-gem 'rails', '~> 6.0.2', '>= 6.0.2.2'
-gem 'puma', '~> 4.1'
+gem 'rails', '~> 6.1.0'
+gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
-gem 'webpacker', '~> 4.0'
+gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # gem 'bcrypt', '~> 3.1.7'
 # gem 'image_processing', '~> 1.2'
 
-gem 'bootsnap', '>= 1.4.2', require: false
+gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'sqlite3', '~> 1.4'
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
 group :development do
-  gem 'web-console', '>= 3.3.0'
-  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'web-console', '>= 4.1.0'
+  gem 'rack-mini-profiler', '~> 2.0'
+  gem 'listen', '~> 3.3'
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
 group :test do
-  gem 'capybara', '>= 2.15'
+  gem 'capybara', '>= 3.26'
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,25 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_063727) do
+ActiveRecord::Schema.define(version: 2020_12_11_181204) do
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "service_name", null: false
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "racers", force: :cascade do |t|
     t.string "name", limit: 255, null: false
@@ -18,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_063727) do
     t.integer "damage", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "race_id"
+    t.bigint "race_id"
     t.integer "rank"
     t.index ["race_id"], name: "index_racers_on_race_id"
   end
@@ -31,8 +41,8 @@ ActiveRecord::Schema.define(version: 2020_04_26_063727) do
   end
 
   create_table "rolls", force: :cascade do |t|
-    t.integer "race_id"
-    t.integer "racer_id"
+    t.bigint "race_id"
+    t.bigint "racer_id"
     t.integer "position", null: false
     t.integer "damage", null: false
     t.string "speed", null: false
@@ -48,4 +58,5 @@ ActiveRecord::Schema.define(version: 2020_04_26_063727) do
     t.index ["racer_id"], name: "index_rolls_on_racer_id"
   end
 
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
