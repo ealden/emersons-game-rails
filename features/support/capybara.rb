a8fde797913e194b7384bd15a5c81729a53f9b97
Ealden Esto E. Escañan <ealden@escanan.com>
2020-12-15 02:37:27 +0800

    Override Capybara default driver with SELENIUM_DRIVER

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,1 +1,6 @@
-Capybara.default_driver = :selenium_chrome_headless
+Capybara.register_driver :safari do |app|
+  Capybara::Selenium::Driver.load_selenium
+  Capybara::Selenium::Driver.new app, browser: :safari
+end
+
+Capybara.default_driver = ENV['SELENIUM_DRIVER']&.to_sym || :selenium_chrome_headless
