842c877e35dc95869681f994acf8d49c5915ce2b
Ealden Esto E. Escañan <ealden@escanan.com>
2021-02-12 17:44:04 +0800

    Default to headless Chrome

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,6 +1,1 @@
-Capybara.register_driver :safari do |app|
-  Capybara::Selenium::Driver.load_selenium
-  Capybara::Selenium::Driver.new app, browser: :safari
-end
-
-Capybara.default_driver = ENV['SELENIUM_DRIVER']&.to_sym || :selenium_chrome_headless
+Capybara.default_driver = :selenium_chrome_headless
